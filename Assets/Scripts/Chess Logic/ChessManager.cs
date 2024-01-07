e28870d455a5e849fbd5a8b8df871d1054e3405e
Isaac Scheff <isaac1996@comcast.net>
2024-01-07 00:24:22 -0500

    Added text input field to game

--- Assets/Scripts/Chess Logic/ChessManager.cs
+++ Assets/Scripts/Chess Logic/ChessManager.cs
@@ -9,6 +9,7 @@ public class ChessManager : MonoBehaviour {
     public static ChessManager Instance { get; private set; }
     [SerializeField] private string _currentFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
     [SerializeField] private GameObject _textBoard;
+    [SerializeField] private TMP_InputField _moveInput;
     private TextMeshProUGUI _boardText;
     void Awake() {
         Instance = this;
