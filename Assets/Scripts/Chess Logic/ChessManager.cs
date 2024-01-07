1b5750e989ff3acd7604c821f2c7fd74c520670a
Isaac Scheff <isaac1996@comcast.net>
2024-01-06 23:22:10 -0500

    Text board correctly updating with FenToBoard and BoardToString methods.

--- Assets/Scripts/Chess Logic/ChessManager.cs
+++ Assets/Scripts/Chess Logic/ChessManager.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChessManager : MonoBehaviour {
     public static ChessManager Instance { get; private set; }
     [SerializeField] private string _currentFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
     [SerializeField] private GameObject _textBoard;
+    private TextMeshProUGUI _boardText;
     void Awake() {
         Instance = this;
+
+        _boardText = _textBoard.GetComponent<TextMeshProUGUI>();
+        _boardText.text = Converters.BoardToString(Converters.FenToBoard(_currentFEN));
+        Debug.Log("_boardText: " + _boardText.text);
     }
   
 }
---
+++ Assets/Scripts/Chess Logic/Converters.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class Converters : MonoBehaviour {
+    public static char[][] FenToBoard(string fen) {
+        string[] rows = fen.Split('/');
+        char[][] board = new char[rows.Length][];
+
+        for (int i = 0; i < rows.Length; i++)
+        {
+            List<char> row = new List<char>();
+            int j = 0;
+
+            while (j < rows[i].Length)
+            {
+                if (char.IsDigit(rows[i][j]))
+                {
+                    int numBlanks = int.Parse(rows[i][j].ToString());
+                    for (int k = 0; k < numBlanks; k++)
+                    {
+                        row.Add('-');
+                    }
+                    j++;
+                }
+                else
+                {
+                    row.Add(rows[i][j]);
+                    j++;
+                }
+            }
+            board[i] = row.ToArray();
+        }
+        return board;
+    }
+
+    public static string BoardToString(char[][] board)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char[] row in board)
+        {
+            sb.Append("['");
+            for (int i = 0; i < row.Length; i++)
+            {
+                sb.Append(row[i]);
+                if (i < row.Length - 1)
+                {
+                    sb.Append("', '");
+                }
+            }
+            sb.AppendLine("']");
+        }
+
+        return sb.ToString();
+    }
+
+
+}
