c60d2e98203840ac98f3cd90fca0a536b9d0fc3a
Isaac Scheff <isaac1996@comcast.net>
2024-01-07 22:57:32 -0500

    Added IsPathClear and IsDiagonalClear functions

--- Assets/Scripts/Chess Logic/LineCertifier.cs
+++ Assets/Scripts/Chess Logic/LineCertifier.cs
@@ -1,6 +1,11 @@
 using System;
 using UnityEngine;
-
+/*
+    These functions assume that the startSquare and endSquare strings are formatted correctly (e.g., "a2", "b4"). 
+    The calculations are based on the ASCII values of the characters, 
+    which works well for this purpose since the letters and numbers in chess notation are consecutive in the ASCII table. 
+    For instance, the difference between 'a' and 'b' is 1, which aligns with their positions on the chessboard
+*/
 public class LineCertifier : MonoBehaviour {
     public static bool StraightVerify(string startSquare, string endSquare) {
         if (startSquare[0] == endSquare[0] || startSquare[1] == endSquare[1])
@@ -17,9 +22,4 @@ public class LineCertifier : MonoBehaviour {
         return Math.Abs(startFile - endFile) == Math.Abs(startRank - endRank);
     }
 }
-/*
-    These functions assume that the startSquare and endSquare strings are formatted correctly (e.g., "a2", "b4"). 
-    The calculations are based on the ASCII values of the characters, 
-    which works well for this purpose since the letters and numbers in chess notation are consecutive in the ASCII table. 
-    For instance, the difference between 'a' and 'b' is 1, which aligns with their positions on the chessboard
-*/
+
--- Assets/Scripts/Chess Logic/PathValidator.cs
+++ Assets/Scripts/Chess Logic/PathValidator.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+/*
+    Character to Index Conversion: Since the board array is 2D, characters from squares 
+    (like 'a', 'b', '1', '2') are converted to array indices. 
+    This involves subtracting 'a' from file characters and '8' from rank characters, 
+    assuming a standard 8x8 chessboard with ranks 1-8 and files a-h
+*/
+public class PathValidator : MonoBehaviour {
+    public bool IsPathClear(string startSquare, string endSquare, char[][] pieces) {
+        int startFile = startSquare[0] - 'a';
+        int startRank = '8' - startSquare[1];
+        int endFile = endSquare[0] - 'a';
+        int endRank = '8' - endSquare[1];
 
-public class PathValidator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        int fileDirection = Math.Sign(endFile - startFile);
+        int rankDirection = Math.Sign(endRank - startRank);
+
+        for (int file = startFile + fileDirection, rank = startRank + rankDirection; 
+            file != endFile || rank != endRank; 
+            file += fileDirection, rank += rankDirection) {
+                if (pieces[rank][file] != '-') {
+                    return false;
+                }
+        }
+        return true;
     }
+    public bool IsDiagonalClear(string startSquare, string endSquare, char[][] pieces) {
+        int startFile = startSquare[0] - 'a';
+        int startRank = '8' - startSquare[1];
+        int endFile = endSquare[0] - 'a';
+        int endRank = '8' - endSquare[1];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int rankDirection = startRank < endRank ? 1 : -1;
+        int fileDirection = startFile < endFile ? 1 : -1;
+
+        for (int rank = startRank + rankDirection, file = startFile + fileDirection; 
+            rank != endRank; 
+            rank += rankDirection, file += fileDirection) {
+                if (pieces[rank][file] != '-') {
+                    return false;
+                }
+        }
+        return true;
     }
 }
+
