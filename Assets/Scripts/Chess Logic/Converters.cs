18e01fc176c2f60674cc858c37b237714f414614
Isaac Scheff <isaac1996@comcast.net>
2024-01-07 21:44:26 -0500

    Added StraightVerify and DiagonalVerify functions

--- Assets/Scripts/Chess Logic/Converters.cs
+++ Assets/Scripts/Chess Logic/Converters.cs
@@ -8,24 +8,19 @@ public class Converters : MonoBehaviour {
         string[] rows = fen.Split('/');
         char[][] board = new char[rows.Length][];
 
-        for (int i = 0; i < rows.Length; i++)
-        {
+        for (int i = 0; i < rows.Length; i++) {
             List<char> row = new List<char>();
             int j = 0;
 
-            while (j < rows[i].Length)
-            {
-                if (char.IsDigit(rows[i][j]))
-                {
+            while (j < rows[i].Length) {
+                if (char.IsDigit(rows[i][j])) {
                     int numBlanks = int.Parse(rows[i][j].ToString());
-                    for (int k = 0; k < numBlanks; k++)
-                    {
+                    for (int k = 0; k < numBlanks; k++) {
                         row.Add('-');
                     }
                     j++;
                 }
-                else
-                {
+                else {
                     row.Add(rows[i][j]);
                     j++;
                 }
@@ -34,19 +29,14 @@ public class Converters : MonoBehaviour {
         }
         return board;
     }
-
-    public static string BoardToString(char[][] board)
-    {
+    public static string BoardToString(char[][] board) {
         StringBuilder sb = new StringBuilder();
 
-        foreach (char[] row in board)
-        {
+        foreach (char[] row in board) {
             sb.Append("['");
-            for (int i = 0; i < row.Length; i++)
-            {
+            for (int i = 0; i < row.Length; i++) {
                 sb.Append(row[i]);
-                if (i < row.Length - 1)
-                {
+                if (i < row.Length - 1) {
                     sb.Append("', '");
                 }
             }
@@ -55,6 +45,4 @@ public class Converters : MonoBehaviour {
 
         return sb.ToString();
     }
-
-
 }
--- Assets/Scripts/Chess Logic/LineCertifier.cs
+++ Assets/Scripts/Chess Logic/LineCertifier.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class LineCertifier : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class LineCertifier : MonoBehaviour {
+    public static bool StraightVerify(string startSquare, string endSquare) {
+        if (startSquare[0] == endSquare[0] || startSquare[1] == endSquare[1])
+            return true;
+        else
+            return false;
     }
+    public bool DiagonalVerify(string startSquare, string endSquare) {
+        char startFile = startSquare[0];
+        char startRank = startSquare[1];
+        char endFile = endSquare[0];
+        char endRank = endSquare[1];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return Math.Abs(startFile - endFile) == Math.Abs(startRank - endRank);
     }
 }
+/*
+    These functions assume that the startSquare and endSquare strings are formatted correctly (e.g., "a2", "b4"). 
+    The calculations are based on the ASCII values of the characters, 
+    which works well for this purpose since the letters and numbers in chess notation are consecutive in the ASCII table. 
+    For instance, the difference between 'a' and 'b' is 1, which aligns with their positions on the chessboard
+*/
